a701d3b681a45aca7ce93f0d057edbca21ea76f7
sferrer <simon.ferrer@apside.com>
2025-01-08 15:37:01 +0100

    learn Doctrine and entityManager

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -6,6 +6,8 @@ use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 class Author
@@ -15,12 +17,16 @@ class Author
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\Length(min:10)]
+    #[Assert\NotBlank()]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Assert\NotBlank()]
     private ?\DateTimeImmutable $dateOfBirth = null;
 
+    #[Assert\GreaterThan(propertyPath:'dateOfBirth')]
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $dateOfDeath = null;
 
